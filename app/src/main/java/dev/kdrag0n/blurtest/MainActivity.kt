401bfd0b9e348f92144b43a39485febf758ecfa5
Danny Lin <danny@kdrag0n.dev>
2021-01-06 03:04:36 -0800

    Keep screen on during rendering

--- app/src/main/java/dev/kdrag0n/blurtest/MainActivity.kt
+++ app/src/main/java/dev/kdrag0n/blurtest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -30,6 +31,8 @@ class MainActivity : AppCompatActivity() {
             controller.hide(WindowInsetsCompat.Type.systemBars())
             controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
+        
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
     companion object {
