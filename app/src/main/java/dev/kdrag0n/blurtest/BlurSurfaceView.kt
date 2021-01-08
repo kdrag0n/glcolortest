c6f0c208c100df4ced63dc3b461c9b88260b45d6
Danny Lin <danny@kdrag0n.dev>
2021-01-07 17:35:57 -0800

    Dither RGB channels only using swizzle

--- app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
+++ app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
@@ -636,10 +636,10 @@ class BlurSurfaceView(context: Context, private val bgBitmap: Bitmap, private va
         out vec4 fragColor;
 
         void main() {
-            vec4 dither = (texture(uDitherTexture, vNoiseUV) - 0.5) / 64.0;
-            vec4 blurred = texture(uBlurredTexture, vUV) + dither;
-            vec4 composition = texture(uCompositionTexture, vUV);
-            fragColor = mix(composition, blurred, 1.0);
+            vec3 dither = (texture(uDitherTexture, vNoiseUV).rgb - 0.5) / 64.0;
+            vec3 blurred = texture(uBlurredTexture, vUV).rgb + dither;
+            vec3 composition = texture(uCompositionTexture, vUV).rgb;
+            fragColor = vec4(mix(composition, blurred, 1.0), 1.0);
         }
         """
     }
