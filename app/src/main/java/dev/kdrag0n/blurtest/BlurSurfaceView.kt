50eb0fa15b5e47a566b4324742f67d7704c912aa
Danny Lin <danny@kdrag0n.dev>
2021-04-13 15:43:02 -0700

    dither: Only dither LSB of output color

--- app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
+++ app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
@@ -714,7 +714,7 @@ class BlurSurfaceView(context: Context, private val bgBitmap: Bitmap, private va
 
         void main() {
             vec3 dither = texture(uDitherTexture, vNoiseUV).rgb * 2.0 - 1.0;
-            dither = sign(dither) * (1.0 - sqrt(1.0 - abs(dither))) * 0.015625;
+            dither = sign(dither) * (1.0 - sqrt(1.0 - abs(dither))) * 0.00392156862745098;
             vec3 blurred = texture(uBlurredTexture, vUV).rgb + dither;
             vec3 composition = texture(uCompositionTexture, vUV).rgb;
             fragColor = vec4(mix(composition, blurred, 1.0), 1.0);
