df947ffefe24b8fa96c879513ea0cb2c7ce3f587
Danny Lin <danny@kdrag0n.dev>
2021-04-13 15:36:13 -0700

    dither: Remap uniform blue noise to triangular PDF

--- app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
+++ app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
@@ -713,7 +713,8 @@ class BlurSurfaceView(context: Context, private val bgBitmap: Bitmap, private va
         out vec4 fragColor;
 
         void main() {
-            vec3 dither = (texture(uDitherTexture, vNoiseUV).rgb - 0.5) * 0.015625;
+            vec3 dither = texture(uDitherTexture, vNoiseUV).rgb * 2.0 - 1.0;
+            dither = sign(dither) * (1.0 - sqrt(1.0 - abs(dither))) * 0.015625;
             vec3 blurred = texture(uBlurredTexture, vUV).rgb + dither;
             vec3 composition = texture(uCompositionTexture, vUV).rgb;
             fragColor = vec4(mix(composition, blurred, 1.0), 1.0);
