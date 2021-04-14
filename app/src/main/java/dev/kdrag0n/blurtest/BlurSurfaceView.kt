ae0a738907862fcc03484ef19df53bc443aea6e6
Danny Lin <danny@kdrag0n.dev>
2021-04-14 00:18:03 -0700

    blur: Render to half-float FBOs

--- app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
+++ app/src/main/java/dev/kdrag0n/blurtest/BlurSurfaceView.kt
@@ -243,7 +243,7 @@ class BlurSurfaceView(context: Context, private val bgBitmap: Bitmap, private va
                     GLFramebuffer(sourceFboWidth shr i, sourceFboHeight shr i, null,
                         GLES31.GL_LINEAR, GLES31.GL_CLAMP_TO_EDGE,
                         // 2-10-10-10 reversed is the only 10-bpc format in GLES 3.1
-                        GLES31.GL_RGB10_A2, GLES31.GL_RGBA, GLES31.GL_UNSIGNED_INT_2_10_10_10_REV
+                        GLES31.GL_RGB16F, GLES31.GL_RGB, GLES31.GL_HALF_FLOAT
                     )
                 )
             }
